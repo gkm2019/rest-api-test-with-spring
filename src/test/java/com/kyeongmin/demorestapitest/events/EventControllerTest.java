996016f809f84d87b2d4eb047c21f16ed9272318
kyeongmin <popo2019@naver.com>
2020-10-02 16:20:44 +0900

    test DB (H2), compile DB (MySQL) && application-test override

--- src/test/java/com/kyeongmin/demorestapitest/events/EventControllerTest.java
+++ src/test/java/com/kyeongmin/demorestapitest/events/EventControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class) //다른 bean 불러와서 사용
+@ActiveProfiles("test")
 public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
